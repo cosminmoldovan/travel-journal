6764276082c23012db43f8cb39f4b586c671df13
Cosmin Moldovan <cosmin.moldovan@protonmail.com>
2022-05-06 11:55:00 +0300

    add design for Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Card from './components/Card';
 import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-     <h1>Travel Journal</h1>
+      <section className="card-list">
+        <Card />
+      </section>
+      
     </div>
   );
 }
